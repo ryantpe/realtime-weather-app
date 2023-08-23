9ae15d3392d0aaaa678571db66d5c0d7b814abdc
ryantpe <ipmoid@gmail.com>
2023-08-23 09:59:24 +0800

    use emotion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
 
 function App() {
   return (
-    <div className='container'>
-      <div className='weather-card'>
-        <h1>Weather</h1>
-      </div>
-    </div>
+    <Container>
+      <WeatherCard>
+        <h1>Weather Card</h1>
+      </WeatherCard>
+    </Container>
   );
 }
 
 export default App;
+ 
